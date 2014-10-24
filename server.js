edf2fbb9f4d9d945cc9623e913e73c86a337a85f
skylersb <skylersb@yahoo.com>
2014-10-24 17:03:24 -0600

    adds user Model and controller

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var database = require('./serverAssets/database');
+// var database = require('./serverAssets/database');
 var port = 3000;
 var cors = require('cors');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+//MONGO SETUP
+var mongoose = require('mongoose');
+var Friend = require('./serverAssets/friend/friendController')
+
+var databaseReference = 'mongodb://localhost/fullStack';
+
+var connection = mongoose.connection;
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/friends', function(req, res){
-	res.send(database.getFriends());
-});
+app.use(express.static(__dirname + '/public'));
 
-app.post('/friends/new' , function(req, res){
-	res.send(database.addFriend(req.body));
-})
+app.get('/friends', Friend.getFriends());
 
 
+app.post('/friends/new' , Friend.addFriend());
 
 
+mongoose.connect(databaseReference);
+connection.once('open', function (){
+	
 app.listen(port, function(){
-	console.log('Listening on port ' + port);
+	console.log('Everything is working and we are connected to our mongoDb on port: ' + port);
+	})
 });
+
